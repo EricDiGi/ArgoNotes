6b18da23b3d502801499e09b520d572e524d06a7
EricDiGi <etd7@students.uwf.edu>
2022-02-15 18:56:58 -0600

    server with sessions

--- Node Server/simpleServer.js
+++ Node Server/simpleServer.js
@@ -1,6 +1,11 @@
 var mysql = require('mysql');
 const express = require('express');
+const sessions = require('express-session');
 const fs = require('fs');
+const crypto = require('crypto');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const res = require('express/lib/response');
 
 //MySQL Connection
 var conn = mysql.createConnection({
@@ -21,6 +26,15 @@ conn.connect(function(err){
 // Build Pipe
 const app = express();
 
+//Session middle-ware
+const oneDay = 1000*60*60*24;
+app.use(sessions({
+    secret: crypto.randomUUID(),
+    saveUninitialized: false,
+    cookie:{maxAge: oneDay},
+    resave: false
+}));
+
 app.use(
     express.urlencoded({
         extended: true
@@ -28,10 +42,13 @@ app.use(
 );
 
 app.use(express.json());
-
 app.use(express.static('web_content'));
+app.use(cookieParser());
+
 
 //Make Workflows
+
+
 app.get("/interactive",(req,res)=>{
     var qs = req.query;
     var cont = req.body;
@@ -47,6 +64,8 @@ app.get("/interactive",(req,res)=>{
 });
 
 app.post("/login", (req,res)=>{
+    var session_info;
+
     var q = "select (\'"+req.body.pword+"\'=user_pass) as auth from accounts where user_acc in  (select uid from users where alias=\'"+req.body.alias+"\' or email=\'"+req.body.alias+"\')";
     conn.query(q,(err, result, fields)=>{
         if (err) console.log(err);
@@ -54,6 +73,11 @@ app.post("/login", (req,res)=>{
             if(result.length == 0) res.redirect('index.html');
             else if(result.length > 1) res.redirect("index1.html");
             else{
+                session_info = req.session;
+                session_info.userActive = true;
+                session_info.visits = 1;
+                console.log(req.session);
+                console.log("User is active: "+ session_info.userActive + " Visits: " + session_info.visits);
                 // HOW???
                 var datagram = {
                     session_id: "", //Security measure
@@ -61,11 +85,23 @@ app.post("/login", (req,res)=>{
                     clusters: [], //Asocciated Clusters
                     notes: [] //User's notes
                 };
-                res.send(datagram);
+                res.redirect("/index");
+                //res.send(req.session);
 
             }
         }
     });
+});
+
+
+// on logout destroy session
+app.get('/logout', (req, res)=>{
+    //destroy session in db also
+    session_info = req.session;
+    session_info.userActive = false;
+    req.session.destroy((err)=>console.log(err));
+    res.redirect('/');
+    console.log("User is active: "+ session_info.userActive + " Visits: " + session_info.visits);
 });
 
 app.post("/signup", (req,res)=>{
@@ -76,8 +112,8 @@ app.post("/signup", (req,res)=>{
             if(err){console.log(err)}
             else{            
                 if (result.length > 0) {
-                    res.send({response_code: 111, response_def: "User Exists", auth_id: result.uid})
-                    //return res.redirect('/login');
+                    //res.send({response_code: 111, response_def: "User Exists", auth_id: result.uid})
+                    return res.redirect('/index1.html');
                 } else {
                     var q = "insert into users (alias, first_name, last_name, dob, email, role_id) values (\'"+req.body.alias+"\',\'"+req.body.first_name+"\',\'"+req.body.last_name+"\',\'"+req.body.dob+"\',\'"+req.body.email+"\',"+parseInt(req.body.role)+");";
                     conn.query(
@@ -105,19 +141,21 @@ app.post("/signup", (req,res)=>{
     );
 });
 
-app.post("/login",(req,res)=>{
-    res.send("This is a login page");
-});
 
 app.get('/',(req,res)=>{
-    res.send("<html><body><h3>This is Argo Notes</h3></body></html>");
+    res.redirect("index.html");
 });
 
+app.get('/index', (req,res)=>{
+    var session_info = req.session;
+    session_info.visits++;
+    res.sendFile("web_content/index.html",{root:__dirname});
+    console.log(session_info);
+});
 
 //Get Server moving
 var server = app.listen(8080,()=>{
     var host = server.address().address
     var port = server.address().port
-
     console.log("Argo Notes Application listening at http://%s:%s",host,port);
 });
